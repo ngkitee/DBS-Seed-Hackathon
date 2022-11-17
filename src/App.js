1fc2d5e6af4841ebc00deffb25f6bfecb4351fa8
Ng Kit Ee <kit@KIT-8.local>
2022-11-17 22:02:00 +0800

    change text to boilerplate

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        kit f1
+        Boilerplate project
         </p>
         <a
           className="App-link"
